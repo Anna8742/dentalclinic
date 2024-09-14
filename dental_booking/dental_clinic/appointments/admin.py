2ed52206ed5f4239b0fda1382ae57f8f0bab997f
Anna <anna.papka014@gmail.com>
2024-09-14 13:36:56 +0100

    Registered the model in the Admin Interface

--- dental_booking/dental_clinic/appointments/admin.py
+++ dental_booking/dental_clinic/appointments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient_name', 'patient_email', 'appointment_date', 'created_at')
+    list_filter = ('appointment_date',)
+    search_fields = ('patient_name', 'patient_email')
