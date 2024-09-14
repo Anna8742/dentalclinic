f444c4b72424bddb64dc804fac0e519a47e0c476
Anna <anna.papka014@gmail.com>
2024-09-14 13:35:56 +0100

    Defined models

--- dental_booking/dental_clinic/appointments/models.py
+++ dental_booking/dental_clinic/appointments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Appointment(models.Model):
+    patient_name = models.CharField(max_length=100)
+    patient_email = models.EmailField()
+    appointment_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.patient_name} - {self.appointment_date}"
+
+    class Meta:
+        ordering = ['appointment_date']
+        unique_together = ('appointment_date',)
